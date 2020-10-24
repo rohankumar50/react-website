7a3cdfe9587d8642baa93a40b83a9c5b177d721b
rohankumarchs50 <rohankumarchs@gmail.com>
2020-10-24 18:36:07 +0530

    Updates

---
+++ precache-manifest.282dd3d12afd0d01cbd15e1f3e0868d6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ca3df0d2a2339c8b37da00c983740e2",
+    "url": "/react-website/index.html"
+  },
+  {
+    "revision": "0cde3d3a4c4fb795278a",
+    "url": "/react-website/static/css/2.974c742c.chunk.css"
+  },
+  {
+    "revision": "f25fea5ff28c1701ea1a",
+    "url": "/react-website/static/css/main.23591431.chunk.css"
+  },
+  {
+    "revision": "0cde3d3a4c4fb795278a",
+    "url": "/react-website/static/js/2.9642efeb.chunk.js"
+  },
+  {
+    "revision": "54e8c4f782a1df2882087d61fdec9005",
+    "url": "/react-website/static/js/2.9642efeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f25fea5ff28c1701ea1a",
+    "url": "/react-website/static/js/main.6a5be8dc.chunk.js"
+  },
+  {
+    "revision": "6a49467c11d17f451514",
+    "url": "/react-website/static/js/runtime-main.e5bca042.js"
+  },
+  {
+    "revision": "c13e5ed6197c36dd9a61e0e6f15e78e9",
+    "url": "/react-website/static/media/undraw_Taking_notes_re_bnaf.c13e5ed6.svg"
+  },
+  {
+    "revision": "fb60b9cca3765949f6eb0cee06037ac0",
+    "url": "/react-website/static/media/undraw_product_iteration_kjok.fb60b9cc.svg"
+  }
+]);
